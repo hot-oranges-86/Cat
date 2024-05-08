c4e24621c71e8b44f16f03a7ee4843f3bb3dc9de
Wojciech <wojciech.stuchlak@gmail.com>
2024-05-08 16:41:45 +0200

    feat: added flags and fixed spelling

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
 )
 
+var numberLines = flag.Bool("n", false, "number all output lines")
+var squeezeBlanks = flag.Bool("s", false, "suppress repeated empty output lines")
+
 func readLines(r io.Reader) {
 	reader := bufio.NewReader(r)
+	lineNum := 1
+	prevBlank := false
+
 	for {
 		line, err := reader.ReadString('\n')
+
 		if err == io.EOF {
 			break
 		}
@@ -20,7 +28,22 @@ func readLines(r io.Reader) {
 			log.Fatal(err)
 		}
 
-		print(line)
+		currBlank := line == "\n" || line == "\r\n"
+
+		// Handle squeezeBlanks flag
+		if *squeezeBlanks && prevBlank && currBlank {
+			continue
+		}
+
+		// Handle numberLines flag
+		if *numberLines {
+			fmt.Printf("%d.\t%s", lineNum, line)
+		} else {
+			fmt.Print(line)
+		}
+
+		lineNum++
+		prevBlank = currBlank
 	}
 }
 
@@ -35,6 +58,7 @@ func cat(filename string) {
 
 func main() {
 	flag.Parse()
-	cat(flag.Arg(0))
-
+	for i := 0; i < flag.NArg(); i++ {
+		cat(flag.Arg(i))
+	}
 }
